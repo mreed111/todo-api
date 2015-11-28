c17ede1c9ade4e980f2d74b1b9e1b751d0988f97
Mike Reed <mike.reed.vallejo@gmail.com>
2015-11-28 09:47:48 -0800

    update

--- server.js
+++ server.js
@@ -1,57 +1,74 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-	//
 	res.send('Todo API Root');
 });
 
 // GET /todos
 app.get('/todos', function (req, res) {
-	//
 	res.json(todos);
 });
 
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-	//
-	var todoId = req.params.id;
-	console.log('todoId = ' + todoId);
-	var i = 0;
-	while (i < todos.length && todos[i].id.toString() !== todoId) {
-		i++;
-	}
-	if (i !== todos.length) {
-		res.json(todos[i]);
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-	//res.send('get ID = ' + req.params.id);
 });
 
-//POST /todos
-app.post('/todos', function (req,res) {
+// POST /todos
+app.post('/todos', function (req, res) {
 	var body = req.body;
-	console.log('description: ' + body.description);
-	
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
 	// add id field
-	body.id = todoNextId;
-	todoNextId++;
-	
+	body.id = todoNextId++;
+
 	// push body into array
 	todos.push(body);
 	
 	res.json(body);
 });
 
+
 app.listen(PORT, function () {
-	//
-	console.log('Express listening on port ' + PORT);
+	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
