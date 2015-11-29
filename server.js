9dc4e91aec5fe542f40473a6ee5acb5549549582
Mike Reed <mike.reed.vallejo@gmail.com>
2015-11-28 16:38:07 -0800

    put/update items

--- server.js
+++ server.js
@@ -49,6 +49,50 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	//
+	//console.log('deleting item ' + req.params.id);
+	
+	if (matchedTodo) {
+		//console.log('...' + matchedTodo.description);
+
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+	
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+	
+	// extend, objects are passed by reference, so the update actually updates the item in todos array.
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
