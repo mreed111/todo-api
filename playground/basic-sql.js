9fc9f87df7f1e85c99e5bc62d2e66733b992ec1b
Mike Reed <mike.reed.vallejo@gmail.com>
2015-11-30 15:05:59 -0800

    sequelize domo

--- playground/basic-sql.js
+++ playground/basic-sql.js
@@ -6,22 +6,42 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
 	description: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+			len: [1, 255]
+		}
 	},
 	completed: {
-		type: Sequelize.BOOLEAN
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
 	}
 });
 
-sequelize.sync().then(function () {
+sequelize.sync({
+	//force: true
+}).then(function () {
 	console.log('Everything is Synced');
 	
-	Todo.create({
-		description: 'Walk my dog',
-		completed: false
-	}).then(function (todo) {
-		console.log('Finnished!');
-		console.log(todo);
+	Todo.findOne({ 
+		where: {id: 2} 
+	}).then(function(todo) {
+		if (todo) {
+			console.log(todo.toJSON());
+		} else {
+			console.log('Item not found.');
+		}
 	});
+	// Todo.create({
+	// 	description: 'Walk my dog',
+	// 	//completed: false
+	// }).then(function (todo) {
+	// 	console.log('Finnished!');
+	// 	console.log(todo);
+	// }).catch(function (e) {
+	// 	console.log('Error: ' + e);
+	// });
 });
 
