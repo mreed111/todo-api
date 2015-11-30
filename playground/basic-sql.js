aa2ec7423a004e2bf46ae298775d07f68468b39f
Mike Reed <mike.reed.vallejo@gmail.com>
2015-11-30 09:47:35 -0800

    basic sql update

---
+++ playground/basic-sql.js
@@ -0,0 +1,27 @@
+var Sequelize =  require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	"dialect": "sqlite",
+	"storage": __dirname + "/basic-sqlite-database.sqlite"
+});
+
+var Todo = sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING
+	},
+	completed: {
+		type: Sequelize.BOOLEAN
+	}
+});
+
+sequelize.sync().then(function () {
+	console.log('Everything is Synced');
+	
+	Todo.create({
+		description: 'Walk my dog',
+		completed: false
+	}).then(function (todo) {
+		console.log('Finnished!');
+		console.log(todo);
+	});
+});
+
