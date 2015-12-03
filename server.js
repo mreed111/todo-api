153988def1618315fdecff223524562ac3c80939
Mike Reed <mike.reed.vallejo@gmail.com>
2015-12-02 22:32:18 -0800

    update DELETE /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -72,18 +72,22 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 1) {
+			res.status(204).send();
+		} else {
+			res.status(404).json({
+				error: 'No todo with ID ' + todoId + ' found.'
+			});
+		}
+	}, function (e) {
+		res.status(500).send();
 	});
-
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
 });
 
 // PUT /todos/:id
