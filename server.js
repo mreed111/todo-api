c730027d72c802a968fa11e3f47cb0dd86f43887
Mike Reed <mike.reed.vallejo@gmail.com>
2015-12-03 16:35:17 -0800

    add sequelize functionality to PUT /todos

--- server.js
+++ server.js
@@ -93,30 +93,34 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	} 
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	} 
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	db.todo.findById(todoId).then(function (todo) {
+		// promise resolved...
+		if (todo) {
+			todo.update(attributes).then(function (todo) {
+				// todo.update promise resolve
+				res.json(todo.toJSON());
+			}, function (e) {
+				// todo.update promise reject
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function () {
+		// promise rejected
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync().then(function() {
